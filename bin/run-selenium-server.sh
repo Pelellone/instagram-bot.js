978403b9573bafa87df13be9dceda632f29e1ddf
Patryk Rzucidło <ptkdev@gmail.com>
2018-01-16 23:24:55 +0100

    [Fix] removed test path

--- bin/run-selenium-server.sh
+++ bin/run-selenium-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xvfb-run --server-args='-screen 0, 1920x1080x16' java -Dwebdriver.chrome.driver="/home/ptkdev/instagram-bot.js/bin/chromedriver" -jar /home/ptkdev/instagram-bot.js/bin/selenium-server-standalone.jar
+xvfb-run --server-args='-screen 0, 1920x1080x16' java -Dwebdriver.chrome.driver="./bin/chromedriver" -jar ./bin/selenium-server-standalone.jar
